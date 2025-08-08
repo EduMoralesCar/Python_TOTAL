cc328d2279a089638b776a24bbc98062e5134add
Edu Morales <165344329+EduMoralesCar@users.noreply.github.com>
2025-08-08 15:12:21 -0500

    Delete src/Dia 5 directory

--- src/Dia 5/Problemas Practicos/Ejercicio02.py
+++
@@ -1,9 +0,0 @@
-def letras_unicas_ordenadas(palabra):
-    palabra = palabra.lower()          # Convierte a minúsculas
-    letras = set(palabra)              # Elimina duplicados
-    return sorted(letras)              # Ordena alfabéticamente
-
-#palabra = input("Ingresa una palabra: ")
-#print(letras_unicas_ordenadas(palabra))
-
-print(letras_unicas_ordenadas(input("Ingresa una palabra: ")))
--- src/Dia 5/Problemas Practicos/Ejercicio03.py
+++
@@ -1,9 +0,0 @@
-def tiene_doble_cero(*args):
-    for i in range(len(args) - 1):
-        if args[i] == 0 and args[i + 1] == 0:
-            return True                         # Encontramos un par de ceros consecutivos
-    return False                                # No se encontraron ceros consecutivos
-
-# Ejemplos de prueba
-print(tiene_doble_cero(5, 6, 1, 0, 0, 9, 3, 5))  # True
-print(tiene_doble_cero(6, 0, 5, 1, 0, 3, 0, 1))  # False
